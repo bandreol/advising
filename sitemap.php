70fee2707dcd03055f21ef89022a060e9f286418
Blaine Neal Andreoli <bandreol@silk2.uvm.edu>
2015-10-19 17:41:36 -0400

    first commit

---
+++ sitemap.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Blaine Andreoli</title>
+        <meta charset="utf-8">
+        <meta name="Blaine Andreoli" content="This is the sitemap for CS 148">
+    </head>
+    <body>
+
+        <h1>Blaine Andreoli</h1>
+        <p><a href="test/index.php">Test</a></p>
+    </body>
+</html>
